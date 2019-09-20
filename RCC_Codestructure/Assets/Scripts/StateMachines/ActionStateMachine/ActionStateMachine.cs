9977caae3085297971eed9bec4b10521caaedd61
Benjamin Rose <rose.benjamin@t-online.de>
2019-09-20 11:24:24 +0200

    rename of statemachine

--- RCC_Codestructure/Assets/Scripts/StateMachines/ActionStateMachine/ActionStateMachine.cs
+++ RCC_Codestructure/Assets/Scripts/StateMachines/ActionStateMachine/ActionStateMachine.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine
+public class ActionExecutioner
 {
     private IState stateCurrent;
     private IState statePrevious;
