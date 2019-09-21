b41f2edfa40171055e50e1c3ac2c7650b1fef71c
Benjamin Rose <rose.benjamin@t-online.de>
2019-09-21 11:06:10 +0200

    Added Extensions to Color and Vector3

---
+++ RCC_Codestructure/Assets/Scripts/Extensions/ColorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions
+{
+    //Get Hex Code
+    public static string ToHex(this Color color)
+    {
+        return "#" + ColorUtility.ToHtmlStringRGBA(color);
+    }
+}
---
+++ RCC_Codestructure/Assets/Scripts/Extensions/ExtensionTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtensionTester : MonoBehaviour
+{
+    [SerializeField]
+    private Transform other;
+
+    void Start()
+    {
+        Color color = Color.red;
+        Debug.Log(color.ToHex());
+
+        transform.position = transform.position.With(y: 0, x: 1);
+
+        Debug.Log(transform.position.DirectionTo(other.position));
+    }
+}
